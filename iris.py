500f634e42449d016647723be913fda8d7fe44e7
Janko Walski <lukaszstawecki@gmail.com>
2021-08-02 18:16:18 +0200

    VIP #1

---
+++ iris.py
@@ -0,0 +1,14 @@
+from sklearn.decomposition import PCA
+
+
+class Iris():
+
+    def __init__(self):
+        pass
+    
+
+
+
+
+if __name__ == "__main__":
+    pass
---
+++ test_iris.py
@@ -0,0 +1,15 @@
+import unittest
+from iris import Iris
+
+class TestMethods(unittest.TestCase):
+
+    def setUp(self):
+        self.program = Iris()
+
+    def test_make_list_method(self):
+        pass
+
+
+
+if __name__ == "__main__":
+    unittest.main()
