bee7619aae01f5798e2f1595ddc7655a6b6bd364
Janko Walski <lukaszstawecki@gmail.com>
2021-09-25 15:22:18 +0200

    #1

--- iris.py
+++ iris.py
@@ -1,14 +1,35 @@
+from csv import reader
 from sklearn.decomposition import PCA
 
+SOURCE_FILE = 'Iris_part.csv'
 
-class Iris():
+class ReadFile():
 
-    def __init__(self):
-        pass
-    
+    def __init__(self, source_file_path, number_of_rows: int=None):
+        self.source_file_path = source_file_path
+        self.number_of_rows = number_of_rows        
+        self.str_list_of_lines = []
 
+    def open_data_file(self):
+        self.data_file = open(SOURCE_FILE, 'r')
+        return self.data_file
 
+    def read_data_file(self):
+        self.data_file_read = reader(self.data_file)
+        return self.data_file_read
+
+    def iterate(self):
+        for row in self.data_file_read:
+            self.str_list_of_lines.append(row)
+        # for i in self.
+
+    def close_file(self):
+        self.data_file.close()
 
 
 if __name__ == "__main__":
-    pass
+    file = ReadFile(SOURCE_FILE)
+    file.open_data_file()
+    file.read_data_file()
+    file.iterate()
+    file.close_file()
--- test_iris.py
+++ test_iris.py
@@ -1,15 +1,26 @@
 import unittest
-from iris import Iris
+from iris import ReadFile
+
+SOURCE_FILE = 'Iris_part.csv'
 
 class TestMethods(unittest.TestCase):
 
     def setUp(self):
-        self.program = Iris()
+        self.program = ReadFile(SOURCE_FILE)
+        self.program.open_data_file()
+        self.program.read_data_file()
+        self.program.iterate()
+        self.program.close_file()
 
-    def test_make_list_method(self):
-        pass
+    # def test_file_type(self):
+    #     self.assertIs(self.program.open_data_file is "<_io.TextIOWrapper name='Iris_part.csv' mode='r' encoding='UTF-8'>")
 
+    def test_reading_lines(self):
+        self.assertEqual(self.program.str_list_of_lines[1], ['1', '5.1', '3.5', '1.4', '0.2', 'Iris-setosa'])
+        self.assertEqual(self.program.str_list_of_lines[2], ['2', '4.9', '3.0', '1.4', '0.2', 'Iris-setosa'])
+        self.assertEqual(self.program.str_list_of_lines[25], ['60', '5.2', '2.7', '3.9', '1.4', 'Iris-versicolor'])
 
+        # SyntaxError('invalid syntax', ('<string>', 1, 1, ))
 
 if __name__ == "__main__":
     unittest.main()
